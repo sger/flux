854330075613fefca5345c8d0e38782295b285f3
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2026-02-23 18:08:35 +0200

    Implement EffectExpr and TypeExpr enums with display functionality

--- src/syntax/token_type.rs
+++ src/syntax/token_type.rs
@@ -121,6 +121,7 @@ define_tokens! {
         Let    => "let",
         Do     => "do",
         Fn     => "fn",
+        With   => "with",
         If     => "if",
         Else   => "else",
         Return => "return",
