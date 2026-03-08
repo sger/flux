2d6513f01c8f022554040f6dc60c2fa7150df7da
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2026-03-08 11:31:52 +0200

    Add diagnostic category type to diagnostics model

---
+++ src/diagnostics/types/diagnostic_category.rs
@@ -0,0 +1,38 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum DiagnosticCategory {
+    ParserDeclaration,
+    ParserDelimiter,
+    ParserSeparator,
+    ParserExpression,
+    ParserPattern,
+    ParserKeyword,
+    NameResolution,
+    ModuleSystem,
+    Orchestration,
+    TypeInference,
+    Effects,
+    RuntimeType,
+    RuntimeExecution,
+    Internal,
+}
+
+impl DiagnosticCategory {
+    pub const fn as_str(self) -> &'static str {
+        match self {
+            Self::ParserDeclaration => "parser_declaration",
+            Self::ParserDelimiter => "parser_delimiter",
+            Self::ParserSeparator => "parser_separator",
+            Self::ParserExpression => "parser_expression",
+            Self::ParserPattern => "parser_pattern",
+            Self::ParserKeyword => "parser_keyword",
+            Self::NameResolution => "name_resolution",
+            Self::ModuleSystem => "module_system",
+            Self::Orchestration => "orchestration",
+            Self::TypeInference => "type_inference",
+            Self::Effects => "effects",
+            Self::RuntimeType => "runtime_type",
+            Self::RuntimeExecution => "runtime_execution",
+            Self::Internal => "internal",
+        }
+    }
+}
